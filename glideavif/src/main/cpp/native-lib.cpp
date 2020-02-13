629988c72ca81029a667506a8acd23e5646dd542
GC <yuuki.inoue@link-u.co.jp>
2020-02-13 19:42:03 +0900

    失敗作

--- glideavif/src/main/cpp/native-lib.cpp
+++ glideavif/src/main/cpp/native-lib.cpp
@@ -8,50 +8,6 @@
 #define TAG               "AvifTest"
 #define LOGD(...)         __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_sample1_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_example_sample1_MainActivity_testAvif(
-        JNIEnv *env,
-        jobject obj,
-        jbyteArray byteArray,
-        int byteArrayLength
-) {
-
-    jbyte *bytes = env->GetByteArrayElements(byteArray, nullptr);
-
-    avifROData raw;
-    raw.data = (const uint8_t *) bytes;
-    raw.size = (size_t) byteArrayLength;
-
-    avifImage *im = avifImageCreateEmpty();
-    avifDecoder *decoder = avifDecoderCreate();
-    avifResult result = avifDecoderRead(decoder, im, &raw);
-
-    if (result != AVIF_RESULT_OK) {
-        LOGD("decode failed");
-        return;
-    }
-
-    LOGD("size: %dx%d", im->width, im->height);
-    LOGD("depth: %d", im->depth);
-    LOGD("yuv format: %d", im->yuvFormat);
-    LOGD("yuvRowBytes: %u %u %u", im->yuvRowBytes[0], im->yuvRowBytes[1], im->yuvRowBytes[2]);
-
-    avifImageYUVToRGB(im);
-
-    LOGD("rgbRowBytes: %u %u %u", im->rgbRowBytes[0], im->rgbRowBytes[1], im->rgbRowBytes[2]);
-
-    avifImageDestroy(im);
-    avifDecoderDestroy(decoder);
-}
-
 extern "C" JNIEXPORT int JNICALL
 Java_jp_co_link_1u_library_glideavif_Avif_decodeAvif(
         JNIEnv *env,
@@ -105,4 +61,73 @@ Java_jp_co_link_1u_library_glideavif_Avif_decodeAvif(
     auto bitmap = new MyBitmap(env, jbitmap);
     bitmap->Load(rgbaList);
     return 0;
+}
+
+extern "C" JNIEXPORT jintArray JNICALL
+Java_jp_co_link_1u_library_glideavif_Avif_decodeAvif2(
+        JNIEnv *env,
+        jobject,
+        jbyteArray sourceData,
+        int sourceDataLength
+) {
+    jbyte *bytes = env->GetByteArrayElements(sourceData, nullptr);
+    if (bytes == nullptr) {
+        LOGD("allocation failed");
+        return nullptr;
+    }
+
+    avifROData raw;
+    raw.data = (const uint8_t *) bytes;
+    raw.size = (size_t) sourceDataLength;
+
+    avifImage *im = avifImageCreateEmpty();
+    avifDecoder *decoder = avifDecoderCreate();
+    avifResult result = avifDecoderRead(decoder, im, &raw);
+
+    env->ReleaseByteArrayElements(sourceData, bytes, JNI_ABORT);
+
+    if (result != AVIF_RESULT_OK) {
+        LOGD("decode failed");
+        return nullptr;
+    }
+    avifImageYUVToRGB(im);
+
+    uint32_t bufferSize = 2 + im->width * im->height;
+    jint *buffer0 = (jint *) malloc(bufferSize * sizeof(jint));
+    if (buffer0 == nullptr) {
+        LOGD("allocation failed");
+        return nullptr;
+    }
+
+    buffer0[0] = im->width;
+    buffer0[1] = im->height;
+
+    auto buffer = buffer0 + 2;
+
+    for (auto i = 0; i < im->height; ++i) {
+        for (auto j = 0; j < im->width; ++j) {
+            uint32_t c = 0xff000000;
+            for (auto k = 0; k < 3; ++k) {
+                uint8_t c0 = im->rgbPlanes[k][i * im->rgbRowBytes[k] + j * 2];
+                uint8_t c1 = im->rgbPlanes[k][i * im->rgbRowBytes[k] + j * 2 + 1];
+                uint32_t c01 = (c0 >> 2) | (c1 << 6);
+                c |= c01 << (8 * (3 - k));
+            }
+            buffer[i * im->width + j] = c;
+        }
+    }
+
+//    if (im->alphaPlane) {
+//        for (int i = 0; i < im->height; ++i) {
+//            for (int j = 0; j < im->width; ++j) {
+//                uint32_t c = im->alphaPlane[i * im->alphaRowBytes + j];
+//                buffer[i * im->width + j] &= c << 24;
+//            }
+//        }
+//    }
+
+    auto returnBuffer = env->NewIntArray(bufferSize);
+    env->SetIntArrayRegion(returnBuffer, 0, bufferSize, buffer0);
+    free(buffer0);
+    return returnBuffer;
 }
--- glideavif/src/main/java/jp/co/link_u/library/glideavif/Avif.kt
+++ glideavif/src/main/java/jp/co/link_u/library/glideavif/Avif.kt
@@ -10,11 +10,31 @@ class Avif(inputStream: InputStream) {
         private set
 
     init {
-        val bitmap = Bitmap.createBitmap(2048, 858, Bitmap.Config.ARGB_8888)
         val byteArray = toByteArray(inputStream)
 
-        decodeAvif(bitmap, byteArray, byteArray.size)
-        this.bitmap = bitmap
+        val colors = decodeAvif2(byteArray, byteArray.size)
+        val width = colors[0]
+        val height = colors[1]
+        this.bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        this.bitmap.setPixels(
+            colors,
+            2,
+            width,
+            0, 0,
+            width,
+            height
+        )
+//        this.bitmap =
+//            Bitmap.createBitmap(
+//                colors,
+//                2,
+//                width,
+//                width,
+//                height,
+//                Bitmap.Config.ARGB_8888
+//            )
+//
+//        this.bitmap.setColorSpace(ColorSpace.get(ColorSpace.Named.ADOBE_RGB))
     }
 
     private external fun decodeAvif(
@@ -22,6 +42,11 @@ class Avif(inputStream: InputStream) {
         byteArray: ByteArray,
         byteArrayLength: Int
     ): Int
+
+    private external fun decodeAvif2(
+        byteArray: ByteArray,
+        byteArrayLength: Int
+    ): IntArray
 
     companion object {
         fun getFromInputStream(s: InputStream): Avif {
