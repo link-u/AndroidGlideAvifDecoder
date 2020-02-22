c10d0ac8ddef2a7a14a77f26959d4f73b6db25f0
psi <ryo.hirafuji@link-u.co.jp>
2020-02-23 03:27:33 +0900

    色の取扱いに関する諸々の修正 ・libyuvの適切な関数を使うようにしました ・YのコピーでOKなのはfull-rangeの時だけなので、その分岐を足しました ・avifImageYUVToRGBがMonochrome画像を入れるともれなく失敗することを発見したので、FIXMEを追加しました

--- glideavif/src/main/cpp/avif_decoder.cpp
+++ glideavif/src/main/cpp/avif_decoder.cpp
@@ -57,7 +57,7 @@ public:
         decoder = avifDecoderCreate();
     }
 
-    ~AvifDecoder() {
+    ~AvifDecoder() noexcept {
         avifDecoderDestroy(decoder);
     }
 
@@ -79,19 +79,60 @@ int (*getYUVConvertFunc(avifImage *im))
          const uint8_t *src_v, int src_stride_v,
          uint8_t *dst_abgr, int dst_stride_abgr,
          int width, int height) {
-    switch (im->yuvFormat) {
-        case AVIF_PIXEL_FORMAT_YUV420:
-            return libyuv::I420ToABGR;
-        case AVIF_PIXEL_FORMAT_YUV422:
-            return libyuv::I422ToABGR;
-        case AVIF_PIXEL_FORMAT_YUV444:
-            return libyuv::I444ToABGR;
+    switch (im->nclx.matrixCoefficients) {
+        // BT601_7_625/BT601_7_525でKrとKbは同じ（See: H.273）
+        case AVIF_NCLX_MATRIX_COEFFICIENTS_BT601_7_625:
+        case AVIF_NCLX_MATRIX_COEFFICIENTS_BT601_7_525:
+            switch (im->yuvFormat) {
+                case AVIF_PIXEL_FORMAT_YUV420:
+                    return libyuv::I420ToABGR;
+                case AVIF_PIXEL_FORMAT_YUV422:
+                    return libyuv::I422ToABGR;
+                case AVIF_PIXEL_FORMAT_YUV444:
+                    return libyuv::I444ToABGR;
+                default:
+                    return nullptr;
+            }
+            break;
+        case AVIF_NCLX_MATRIX_COEFFICIENTS_BT709:
+            switch (im->yuvFormat) {
+                case AVIF_PIXEL_FORMAT_YUV420:
+                    return libyuv::H420ToABGR;
+                case AVIF_PIXEL_FORMAT_YUV422:
+                    return libyuv::H422ToABGR;
+                case AVIF_PIXEL_FORMAT_YUV444:
+                    return libyuv::H444ToABGR;
+                default:
+                    return nullptr;
+            }
+            break;
+       case AVIF_NCLX_MATRIX_COEFFICIENTS_BT2020_NCL:
+           switch (im->yuvFormat) {
+               case AVIF_PIXEL_FORMAT_YUV420:
+                   return libyuv::U420ToABGR;
+               case AVIF_PIXEL_FORMAT_YUV422:
+                   return libyuv::U422ToABGR;
+               case AVIF_PIXEL_FORMAT_YUV444:
+                   return libyuv::U444ToABGR;
+               default:
+                   return nullptr;
+           }
+           break;
         default:
+            // 他のColorMatrixはlibyuvは現在非対応
             return nullptr;
     }
 }
-
-void convert8bitYUVTORGB(JNIEnv *env, avifImage *im, std::vector<uint8_t> &rgbaList) {
+void convert8bitLimitedYUVTORGB(JNIEnv *env, avifImage *im, std::vector<uint8_t> &rgbaList) {
+    if (isGrayscale(im)) {
+        auto result = libyuv::I400ToARGB(im->yuvPlanes[0], im->yuvRowBytes[0],
+                                         rgbaList.data(), im->width * 4,
+                                         im->width, im->height);
+        if (result != 0) {
+            throwException(env, "convert yuv to rgb with libyuv failed");
+        }
+        return;
+    }
     auto convertYUV = getYUVConvertFunc(im);
     if (convertYUV) {
         auto result = convertYUV(
@@ -101,12 +142,12 @@ void convert8bitYUVTORGB(JNIEnv *env, avifImage *im, std::vector<uint8_t> &rgbaL
                 rgbaList.data(), im->width * 4,
                 im->width, im->height);
         if (result != 0) {
-            throwException(env, "convert yuv to rgb with libyuv failed");
+            throwException(env, "convert 8bpc limited yuv to rgb with libyuv failed");
         }
     } else {
         auto result = avifImageYUVToRGB(im);
         if (result != AVIF_RESULT_OK) {
-            throwException(env, "convert yuv to rgb failed");
+            throwException(env, "convert 8bpc limited yuv to rgb with libavif failed");
         }
 
         copyColorPixels<uint8_t>(env, im, rgbaList);
@@ -138,28 +179,41 @@ jobject decodeAvif(JNIEnv *env, jbyteArray sourceData, int sourceDataLength) {
     auto im = decoder->image;
     std::vector<uint8_t> rgbaList(im->width * im->height * 4);
 
-    if (isGrayscale(im)) {
-        switch (im->depth) {
-            case 8:
-                copyGrayscalePixels<uint8_t>(env, im, rgbaList);
-                break;
-            case 10:
-            case 12:
-                copyGrayscalePixels<uint16_t>(env, im, rgbaList);
-                break;
-            default:
-                throwException(env, "unknown color depth");
+    if(im->nclx.fullRangeFlag) {
+        if (isGrayscale(im)) {
+            // fullRangeかつGrayscaleの時だけは、Yの値をそのままコピーしてよい
+            // FIXME(ledyba-z):
+            //  transferCharacteristicsとmatrixCoefficientsの組み合わせによっては駄目な可能性がある
+            switch (im->depth) {
+                case 8:
+                    copyGrayscalePixels<uint8_t>(env, im, rgbaList);
+                    break;
+                case 10:
+                case 12:
+                    copyGrayscalePixels<uint16_t>(env, im, rgbaList);
+                    break;
+                default:
+                    throwException(env, "unknown color depth");
+            }
+        } else {
+            result = avifImageYUVToRGB(im);
+            if (result != AVIF_RESULT_OK) {
+                throwException(env, "convert 8bpc full yuv to rgb with libavif failed");
+            }
+
+            copyColorPixels<uint16_t>(env, im, rgbaList);
         }
     } else {
         switch (im->depth) {
             case 8:
-                convert8bitYUVTORGB(env, im, rgbaList);
+                convert8bitLimitedYUVTORGB(env, im, rgbaList);
                 break;
             case 10:
             case 12:
+                // FIXME(ledyba-z): avifImageYUVToRGBがMonochromeを一切考慮していない
                 result = avifImageYUVToRGB(im);
                 if (result != AVIF_RESULT_OK) {
-                    throwException(env, "convert yuv to rgb failed");
+                    throwException(env, "convert high bit-depth limited yuv to rgb with libavif failed");
                 }
 
                 copyColorPixels<uint16_t>(env, im, rgbaList);
