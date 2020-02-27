1c38f69d6fef6013061135a793187bc1ef5a383f
GC <yuuki.inoue@link-u.co.jp>
2020-02-27 15:58:32 +0900

    オレオレ認証を通す

--- app/src/main/java/jp/co/link_u/library/glideavif/sample/AvifModule.kt
+++ app/src/main/java/jp/co/link_u/library/glideavif/sample/AvifModule.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import jp.co.link_u.library.glideavif.AvifDecoder
 import java.io.InputStream
@@ -17,6 +19,11 @@ class AvifModule : AppGlideModule() {
                 InputStream::class.java,
                 Bitmap::class.java,
                 AvifDecoder()
+            )
+            .replace(
+                GlideUrl::class.java,
+                InputStream::class.java,
+                OkHttpUrlLoader.Factory(UnsafeOkHttpClient.getUnsafeOkHttpClient())
             )
     }
 
