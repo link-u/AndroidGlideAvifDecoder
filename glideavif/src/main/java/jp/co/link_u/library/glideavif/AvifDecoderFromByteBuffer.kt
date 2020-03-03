20026f428028248d73268a256a2153c2306e0768
GC <yuuki.inoue@link-u.co.jp>
2020-03-03 13:34:51 +0900

    ヘッダからAVIFかどうかを判別する

--- glideavif/src/main/java/jp/co/link_u/library/glideavif/AvifDecoderFromByteBuffer.kt
+++ glideavif/src/main/java/jp/co/link_u/library/glideavif/AvifDecoderFromByteBuffer.kt
@@ -10,7 +10,7 @@ import java.nio.ByteBuffer
 
 class AvifDecoderFromByteBuffer : ResourceDecoder<ByteBuffer, Bitmap> {
     override fun handles(source: ByteBuffer, options: Options): Boolean {
-        return true
+        return isAvif(source)
     }
 
     override fun decode(
@@ -29,6 +29,19 @@ class AvifDecoderFromByteBuffer : ResourceDecoder<ByteBuffer, Bitmap> {
         }
     }
 
+    private fun isAvif(buf: ByteBuffer): Boolean {
+        try {
+            while (buf.hasRemaining()) {
+                val box = HeaderBox(buf)
+                if (box.type.contentEquals(HeaderBox.ftyp)) {
+                    return box.data.take(4).toByteArray().contentEquals(HeaderBox.avif)
+                }
+            }
+        } catch (e: Throwable) {
+        }
+        return false
+    }
+
     private external fun decodeAvif(
         byteBuffer: ByteBuffer,
         byteBufferLength: Int
@@ -40,6 +53,37 @@ class AvifDecoderFromByteBuffer : ResourceDecoder<ByteBuffer, Bitmap> {
             System.loadLibrary("avif")
             System.loadLibrary("yuv")
             System.loadLibrary("avif_decoder")
+        }
+    }
+
+    class HeaderBox(buf: ByteBuffer) {
+        var size: Int = 0
+            private set
+        val type: ByteArray = ByteArray(4)
+        var data: ByteArray
+            private set
+
+        init {
+            size = buf.getInt()
+
+            if (size == 1 || size == 0) {
+                throw RuntimeException("invalid size")
+            }
+
+            buf.get(type)
+
+            if (!primitives.any { type.contentEquals(it) }) {
+                throw RuntimeException("unknown box")
+            }
+
+            data = ByteArray(size - 8)
+            buf.get(data)
+        }
+
+        companion object {
+            val ftyp = byteArrayOf(0x66, 0x74, 0x79, 0x70)
+            val avif = byteArrayOf(0x61, 0x76, 0x69, 0x66)
+            val primitives = arrayOf(ftyp)
         }
     }
 }
