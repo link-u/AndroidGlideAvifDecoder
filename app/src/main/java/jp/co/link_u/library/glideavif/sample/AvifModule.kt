30b3c00cde739e08c9e9b05a10262e19d001a7db
GC <yuuki.inoue@link-u.co.jp>
2020-03-11 16:08:35 +0900

    remove unnecessary file

--- app/src/main/java/jp/co/link_u/library/glideavif/sample/AvifModule.kt
+++ app/src/main/java/jp/co/link_u/library/glideavif/sample/AvifModule.kt
@@ -5,11 +5,8 @@ import android.graphics.Bitmap
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
-import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import jp.co.link_u.library.glideavif.AvifDecoderFromByteBuffer
-import java.io.InputStream
 import java.nio.ByteBuffer
 
 @GlideModule
@@ -20,11 +17,6 @@ class AvifModule : AppGlideModule() {
                 ByteBuffer::class.java,
                 Bitmap::class.java,
                 AvifDecoderFromByteBuffer()
-            )
-            .replace(
-                GlideUrl::class.java,
-                InputStream::class.java,
-                OkHttpUrlLoader.Factory(UnsafeOkHttpClient.getUnsafeOkHttpClient())
             )
     }
 
